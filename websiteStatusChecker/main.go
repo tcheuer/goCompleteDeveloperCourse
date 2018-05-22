467b36ebdcbba78fc86f4492b4c9947dd44a0e93
Thomas Heuer <tcheuer@gmail.com>
2018-05-22 17:18:29 -0400

    Added basic go routine support, not entirely correct

--- websiteStatusChecker/main.go
+++ websiteStatusChecker/main.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 )
 
+//Concurrency is not parallelism. Concurrency says that any go routine can
+//execute in any order. Parallelism means that multiple go routines can execute
+//at the same time.  Parallelism requires multiple cores.
 func main() {
 	links := []string{
 		"http://www.google.com",
@@ -14,19 +17,29 @@ func main() {
 		"http://amazon.com",
 	}
 
+	//Remember channels can only work with single type.
+	//The channel must be passed into the printSiteStatus function,
+	//otherwise it will not be able to use it to communicate.
+	c := make(chan string)
+
 	for _, link := range links {
-		printSiteStatus(link)
+		//The go keyord is only used in front of function calls
+		go printSiteStatus(link, c)
 	}
+
+	fmt.Println(<-c)
 
 }
 
-func printSiteStatus(address string) {
-	_, err := http.Get(address)
+func printSiteStatus(address string, c chan string) {
+	_, err := http.Get(address) //Blocking call!
 
 	if err != nil {
 		fmt.Printf("%v connection error!!! \t\t%v\n", address, err)
+		c <- "May be down"
 	} else {
 		fmt.Printf("%v connection successful!\n", address)
+		c <- "It is up"
 	}
 	return
 }
